d4ad7d3e6c80eb5c9528c444a03bac916a49ed34
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2009-09-16 19:52:33 +0000

    *** empty log message ***

--- src/br/ufma/deinf/laws/ncl/help/NCLHelper.java
+++ src/br/ufma/deinf/laws/ncl/help/NCLHelper.java
@@ -1,59 +1,95 @@
+/*******************************************************************************
+ * This file is part of the authoring environment in Nested Context Language -
+ * NCL Eclipse.
+ * 
+ * Copyright: 2007-2009 UFMA/LAWS (Laboratory of Advanced Web Systems), All Rights Reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * For further information contact:
+ * 		ncleclipse@laws.deinf.ufma.br
+ * 		http://www.laws.deinf.ufma.br/ncleclipse
+ * 		http://www.laws.deinf.ufma.br
+ ********************************************************************************/
 package br.ufma.deinf.laws.ncl.help;
+
 import java.util.Map;
 
-
+/**
+ * 
+ * @author Roberto Azevedo <roberto@laws.deinf.ufma.br>
+ * 
+ */
 public class NCLHelper {
 
-	private static Map<String, Map<String,String> > helpMap;
+	private static Map<String, Map<String, String>> helpMap;
 	private static NCLHelperFactory helpFactory;
 	private static String helpFileName;
 	private static NCLHelper instance;
-	
-	private NCLHelper(){
+
+	private NCLHelper() {
 	}
-	
-	public static NCLHelper getNCLHelper(){
-		if(instance == null)
+
+	public static NCLHelper getNCLHelper() {
+		if (instance == null)
 			instance = new NCLHelper();
 		return instance;
 	}
-	
-	public static void buildHelp(){
+
+	public static void buildHelp() {
 		helpFactory = new NCLHelperFactory(helpFileName);
 		helpFactory.buildHelp();
 		helpMap = helpFactory.getHelp();
 	}
+
 	/**
-	 * Pega uma descrição de help para um atributo de um elemento especifico, retorna NULL 
-	 * caso não consiga encontrar nenhuma descrição para este atributo
-	 * @param element - o elemento a ser procurado
-	 * @param attribute - o atributo deste elemento
+	 * Pega uma descrição de help para um atributo de um elemento especifico,
+	 * retorna NULL caso não consiga encontrar nenhuma descrição para este
+	 * atributo
+	 * 
+	 * @param element
+	 *            - o elemento a ser procurado
+	 * @param attribute
+	 *            - o atributo deste elemento
 	 * @return description - a descrição de help do atributo especifico
 	 */
-	public String getHelpDescription(String element, String attribute){
+	public String getHelpDescription(String element, String attribute) {
 		String description = "";
-		Map<String,String> attMap;
-		if(helpMap == null) return description;
+		Map<String, String> attMap;
+		if (helpMap == null)
+			return description;
 		attMap = helpMap.get(element);
-		if (attMap != null){
+		if (attMap != null) {
 			description = attMap.get(attribute);
 		}
 		return description;
 	}
-	
+
 	/**
-	 * Pega uma descrição de help para um elemento especifico, retorna NULL 
+	 * Pega uma descrição de help para um elemento especifico, retorna NULL
 	 * caso não consiga encontrar nenhuma descrição para este elemento
-	 * @param element - o elemento a ser procurado
+	 * 
+	 * @param element
+	 *            - o elemento a ser procurado
 	 * @return description - a descrição de help do elemento especifico
 	 */
-	public static String getHelpDescription(String element){
+	public static String getHelpDescription(String element) {
 		String attribute = "self";
 		String description = "";
-		Map<String,String> attMap;
-		if(helpMap == null) return description;
+		Map<String, String> attMap;
+		if (helpMap == null)
+			return description;
 		attMap = helpMap.get(element);
-		if (attMap != null){
+		if (attMap != null) {
 			description = attMap.get(attribute);
 		}
 		return description;
@@ -66,14 +102,15 @@ public class NCLHelper {
 	public String getHelpFileName() {
 		return helpFileName;
 	}
-	
-	public static void main(String []args){
+
+	public static void main(String[] args) {
 		NCLHelper helper = NCLHelper.getNCLHelper();
 		helper.setHelpFileName("resources/help.txt");
 		helper.buildHelp();
-		System.out.println("ncl = " +helper.getHelpDescription("ncl"));
-		System.out.println("head = " +helper.getHelpDescription("head"));
-		System.out.println("body = " +helper.getHelpDescription("body"));
-		System.out.println("head, id = " +helper.getHelpDescription("head", "id"));
+		System.out.println("ncl = " + helper.getHelpDescription("ncl"));
+		System.out.println("head = " + helper.getHelpDescription("head"));
+		System.out.println("body = " + helper.getHelpDescription("body"));
+		System.out.println("head, id = "
+				+ helper.getHelpDescription("head", "id"));
 	}
 }
--- src/br/ufma/deinf/laws/ncl/help/NCLHelperFactory.java
+++ src/br/ufma/deinf/laws/ncl/help/NCLHelperFactory.java
@@ -1,4 +1,27 @@
+/*******************************************************************************
+ * This file is part of the authoring environment in Nested Context Language -
+ * NCL Eclipse.
+ * 
+ * Copyright: 2007-2009 UFMA/LAWS (Laboratory of Advanced Web Systems), All Rights Reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * For further information contact:
+ * 		ncleclipse@laws.deinf.ufma.br
+ * 		http://www.laws.deinf.ufma.br/ncleclipse
+ * 		http://www.laws.deinf.ufma.br
+ ********************************************************************************/
 package br.ufma.deinf.laws.ncl.help;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,67 +34,78 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * @author Roberto Azevedo <roberto@laws.deinf.ufma.br>
+ * 
+ */
 public class NCLHelperFactory {
 
 	private String helpFileName;
 	private String fileBuffer;
-	private Map<String, Map<String,String>> help;
+	private Map<String, Map<String, String>> help;
 
 	public NCLHelperFactory(String Filename) {
 		this.helpFileName = Filename;
 	}
-	
+
 	/**
 	 * Constroi o Help e coloca em um Map<String, Map<String,String>>
 	 */
-	public void buildHelp(){
+	public void buildHelp() {
 		readHelperFileIntoString();
 		parseHelperFileIntoMap();
 	}
-	
+
 	/**
 	 * 
-	 * @param buffer - Buffer representando o arquivo de help
-	 * @return helpMap - O map que terá como chave o nome do elemento e como valor outro Map que irá mapear <atributo, descrição>
+	 * @param buffer
+	 *            - Buffer representando o arquivo de help
+	 * @return helpMap - O map que terá como chave o nome do elemento e como
+	 *         valor outro Map que irá mapear <atributo, descrição>
 	 */
-	private void parseHelperFileIntoMap(){
-		
-		help = new HashMap<String, Map<String,String>>();
+	private void parseHelperFileIntoMap() {
+
+		help = new HashMap<String, Map<String, String>>();
 		Map<String, String> descriptionMap;
-		StringTokenizer st = new StringTokenizer(fileBuffer,"|");
-		String element,attribute,description;
-		
-		while(st.hasMoreTokens()) {
+		StringTokenizer st = new StringTokenizer(fileBuffer, "|");
+		String element, attribute, description;
+
+		while (st.hasMoreTokens()) {
 			/* pega o elemento atributo e sua descrição */
-			element = st.nextToken().trim();			
+			element = st.nextToken().trim();
 			System.out.println(element);
 			attribute = st.nextToken().trim();
 			System.out.println(attribute);
 			description = st.nextToken().trim();
 			/* tenta pegar o Map correspondente a esse elemento */
 			descriptionMap = help.get(element);
-			
-			if ( descriptionMap != null ){ 
+
+			if (descriptionMap != null) {
 				/* se existir adiciona a descrição para o novo atributo */
 				descriptionMap.put(attribute, description);
 			} else {
-				/* se não existir cria o novo Map e adiciona a descrição desse atributo */
-				descriptionMap = new HashMap<String,String>();
+				/*
+				 * se não existir cria o novo Map e adiciona a descrição
+				 * desse atributo
+				 */
+				descriptionMap = new HashMap<String, String>();
 				descriptionMap.put(attribute, description);
 				help.put(element, descriptionMap);
 			}
 		}
-		
+
 	}
 
 	/**
 	 * Abre o arquivo de helper e coloca dentro de um BufferString
+	 * 
 	 * @return buffer - A string de buffer que conterá o arquivo de help
 	 */
 	private void readHelperFileIntoString() {
-		
+
 		try {
-			
+
 			File helperFile = new File(this.helpFileName);
 			InputStreamReader input = new InputStreamReader(
 					new FileInputStream(helperFile), "UTF-8");
@@ -81,7 +115,7 @@ public class NCLHelperFactory {
 			CharBuffer charBuffer = CharBuffer.allocate((int) helperFile
 					.length());
 			in.read(charBuffer);
-			
+
 			this.fileBuffer = new String(charBuffer.array());
 
 		} catch (UnsupportedEncodingException e) {
@@ -95,14 +129,13 @@ public class NCLHelperFactory {
 			e.printStackTrace();
 		}
 
-		
 	}
 
-	public void setHelp(Map<String, Map<String,String>> help) {
+	public void setHelp(Map<String, Map<String, String>> help) {
 		this.help = help;
 	}
 
-	public Map<String, Map<String,String>> getHelp() {
+	public Map<String, Map<String, String>> getHelp() {
 		return help;
 	}
 
